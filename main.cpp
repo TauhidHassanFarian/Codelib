abd688536196762e30292a272b1da6a72f8edab1
Tauhid Hassan Farian <56789861+TauhidHassanFarian@users.noreply.github.com>
2024-05-30 01:21:21 +0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+ 
+#define speedup ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+ 
+const ll mod = 1e9+7;
+const ll inf = 1e18;
+const ll inf2 = 1e9;
+
+int main()
+{
+    speedup;
+	int t = 1;
+	//cin >> t;
+    while(t--){
+        
+    }
+	return 0;
+}
