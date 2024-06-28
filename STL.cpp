6107156549be50fbd4ede301ef9b046b09320402
Tauhid Hassan Farian <56789861+TauhidHassanFarian@users.noreply.github.com>
2024-06-29 01:42:19 +0600

    Update STL.cpp

--- STL.cpp
+++ STL.cpp
@@ -45,7 +45,7 @@ String:
 bitset: 
     bitset<10> s; ->bitset of 10 elements. each takes 1 bit of memory. the value can be 0 or 1
     bitset<10> s(string("0010011010")); -> defining all bits at the same time
-    if a and b pre defined bitset then we can use bitwise operations and get bit string as output -> cout << a&b; -> 0010101010(sample output)
+    if a and b pre-defined bitset then we can use bitwise operations and get bit string as output -> cout << a&b; -> 0010101010(sample output)
     & -> and; | -> or; ^ -> xor
 
 deque: 
@@ -71,6 +71,35 @@ Queue:
     q.pop() -> removes the first element
 
 priority queue:
+    O(logn)
+    priority_queue<int> q; -> largest element first
+    priority_queue<int,vector<int>,greater<int>> q; -> smallest element first
+    q.push()
+    q.top()
+    q.pop()
+
+Policy-based data structures:(sorted)
+    O(logn)
+    {    #include <ext/pb_ds/assoc_container.hpp>
+         #include <ext/pb_ds/tree_policy.hpp>
+         using namespace __gnu_pbds;
+    } -> use this three-line start of the program
+    
+    #define ordered_set tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> -> have to change int to other data type if needed
+    
+    ordered_set s;
+    s.insert()
+    {
+        auto x = s.find_by_order(4);
+        cout << *x << "\n";
+    } -> find the 4th element
+    {
+        cout << s.order_of_key(7) << "\n";
+    } -> find the position of the element. if the element if not in set then if will return the  position that the element would have in the set
+
+
+
+    
     
 
 
